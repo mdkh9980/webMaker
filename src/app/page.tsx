660f9894b8c1cd4e0cc59d519e96ff95a9f14379
MD Hussain <mdkh.9980@gmail.com>
2025-07-24 11:02:30 +0530

    configured open ai model to inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,26 @@
 "use client";
 
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
-export default function Home() {
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
 
+export default function Home() {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const data = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
       toast.success("Background Job Invoked");
-    },
-    onError: () => {
-      toast.error("Failed to invoke background job");
-    },
+    }
   }));
 
   return (
     <>
       <div className="p-4 max-w-7xl mx-auto">
-        <Button disabled={data.isPending} onClick={() => data.mutate({ text: "client" })}>
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button disabled={data.isPending} onClick={() => data.mutate({ value: value })}>
           <p>Invoke Background Job</p>
         </Button>
       </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,19 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const summarize = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer.  You summarize in 5 words.",
+      model: openai({ model: "gpt-4.1-nano"}),
+    });
+
+    const { output } = await summarize.run(
+      `Summarize the following text : ${event.data.value}`
+    );
+    return output
+  }
 );
